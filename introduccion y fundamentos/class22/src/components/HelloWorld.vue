dd893895f7dba6a5e8f0f6b31a07368717d8c1f2
anavalca <avc92j@gmail.com>
2022-11-24 13:02:56 +0100

    create part2 file

---
+++ introduccion y fundamentos/class22/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Holaaa",
+    };
+  },
+};
+</script>
+
+<style>
+p {
+  color: rgb(237, 151, 39);
+}
+</style>
